0d28842e0d6eb54f39632761936effd5f4ae3da6
Yang Bo <atryyang@thoughtworks.com>
2019-03-27 15:55:46 +0800

    Provide BindableSeq type class for Array, Seq, and js.Array

--- shared/build.sbt
+++ shared/build.sbt
@@ -4,4 +4,6 @@ libraryDependencies += "com.thoughtworks.binding" %%% "binding" % "11.7.0"
 
 libraryDependencies += "com.github.mpilquist" %%% "simulacrum" % "0.15.0"
 
+libraryDependencies += "com.thoughtworks.enableIf" %% "enableif" % "1.1.6"
+
 addCompilerPlugin("org.scalamacros" % "paradise" % "2.1.1" cross CrossVersion.full)
--- shared/src/main/scala/com/thoughtworks/binding/bindable/package.scala
+++ shared/src/main/scala/com/thoughtworks/binding/bindable/package.scala
@@ -10,6 +10,42 @@ package object bindable extends Bindable.ToBindableOps with BindableSeq.ToBindab
 }
 
 package bindable {
+  import com.thoughtworks.enableMembersIf
+  @enableMembersIf(c => !c.compilerSettings.exists(_.matches("""^-Xplugin:.*scalajs-compiler_[0-9\.\-]*\.jar$""")))
+  private[bindable] object Jvm {
+    trait LowPriorityJsBindableSeq2 extends LowPriorityBindableSeq2
+    trait LowPriorityJsBindableSeq0 extends LowPriorityBindableSeq0
+  }
+
+  @enableMembersIf(c => c.compilerSettings.exists(_.matches("""^-Xplugin:.*scalajs-compiler_[0-9\.\-]*\.jar$""")))
+  private[bindable] object Js {
+    import scala.scalajs.js
+
+    trait LowPriorityJsBindableSeq2 extends LowPriorityBindableSeq2 {
+
+      implicit def jsArrayBindableSeq[Value0]: BindableSeq.Aux[js.Array[Value0], Value0] =
+        new BindableSeq[js.Array[Value0]] {
+          type Value = Value0
+          def toBindingSeq(from: js.Array[Value0]): BindingSeq[Value] = Constants(from: _*)
+        }
+    }
+
+    trait LowPriorityJsBindableSeq0 extends LowPriorityBindableSeq0 {
+
+      implicit def bindingJsArrayBindableSeq[Value0]: BindableSeq.Aux[Binding[js.Array[Value0]], Value0] =
+        new BindableSeq[Binding[js.Array[Value0]]] {
+          type Value = Value0
+          def toBindingSeq(from: Binding[js.Array[Value0]]): BindingSeq[Value] =
+            Constants(from).flatMap { from =>
+              Constants(from.bind: _*)
+            }
+        }
+    }
+
+  }
+
+  import Jvm._
+  import Js._
 
   private[bindable] trait LowPriorityBindable0 {
 
@@ -92,18 +128,30 @@ package bindable {
   private[bindable] trait LowPriorityBindableSeq2 {
     @deprecated("Potential naming conflict with `Bindable.constantBindable`.", "1.0.2")
     private[bindable] def constantBindable[Value] = constantsBindableSeq[Value]
-    
+
     implicit def constantsBindableSeq[Value0]: BindableSeq.Aux[Value0, Value0] = new BindableSeq[Value0] {
       type Value = Value0
       def toBindingSeq(from: Value): BindingSeq[Value] = Constants(from)
     }
   }
 
-  private[bindable] trait LowPriorityBindableSeq1 extends LowPriorityBindableSeq2 {
+  private[bindable] trait LowPriorityBindableSeq1 extends LowPriorityJsBindableSeq2 {
     implicit def bindingBindableSeq[Value0]: BindableSeq.Aux[Binding[Value0], Value0] =
       new BindableSeq[Binding[Value0]] {
         type Value = Value0
         def toBindingSeq(from: Binding[Value0]): BindingSeq[Value] = SingletonBindingSeq(from)
+      }
+
+    implicit def scalaSeqBindableSeq[Value0]: BindableSeq.Aux[Seq[Value0], Value0] =
+      new BindableSeq[Seq[Value0]] {
+        type Value = Value0
+        def toBindingSeq(from: Seq[Value0]): BindingSeq[Value] = Constants(from: _*)
+      }
+
+    implicit def scalaArrayBindableSeq[Value0]: BindableSeq.Aux[Array[Value0], Value0] =
+      new BindableSeq[Array[Value0]] {
+        type Value = Value0
+        def toBindingSeq(from: Array[Value0]): BindingSeq[Value] = Constants(from: _*)
       }
   }
 
@@ -116,7 +164,7 @@ package bindable {
       }
   }
 
-  object BindableSeq extends LowPriorityBindableSeq0 {
+  object BindableSeq extends LowPriorityJsBindableSeq0 {
     type Aux[-From, Value0] = BindableSeq[From] {
       type Value = Value0
     }
@@ -130,6 +178,24 @@ package bindable {
         type Value = Value0
         def toBindingSeq(from: Binding[BindingSeq[Value0]]): BindingSeq[Value] =
           Constants(from).flatMapBinding(identity)
+      }
+
+    implicit def bindingScalaArrayBindableSeq[Value0]: Aux[Binding[Array[Value0]], Value0] =
+      new BindableSeq[Binding[Array[Value0]]] {
+        type Value = Value0
+        def toBindingSeq(from: Binding[Array[Value0]]): BindingSeq[Value] =
+          Constants(from).flatMap { from =>
+            Constants(from.bind: _*)
+          }
+      }
+
+    implicit def bindingScalaSeqBindableSeq[Value0]: Aux[Binding[Seq[Value0]], Value0] =
+      new BindableSeq[Binding[Seq[Value0]]] {
+        type Value = Value0
+        def toBindingSeq(from: Binding[Seq[Value0]]): BindingSeq[Value] =
+          Constants(from).flatMap { from =>
+            Constants(from.bind: _*)
+          }
       }
 
   }
@@ -174,22 +240,27 @@ package bindable {
     *          }.watch()
     *          }}}
     *
-    *          And the `mySection` method also accepts `Node` or `BindingSeq[Node]` parameter.
+    *          And the `mySection` method also accepts `Node`, `BindingSeq[Node]`, `List[Node]`, `Array[Node]`, `js.Array[Node]`, or `Binding[js.Array[Node]]` parameter.
     *          {{{
+    *          import scala.scalajs.js
     *          @dom def test = {
     *            mySection(<button type="button">My Button 3</button>).bind.innerHTML should be("<button type=\"button\">My Button 3</button>")
     *            mySection(<button type="button">My Button 4</button><button type="button">My Button 5</button>).bind.innerHTML should be(
     *              "<button type=\"button\">My Button 4</button><button type=\"button\">My Button 5</button>"
     *            )
+    *            mySection(List(<button type="button">My Button 6</button>, <button type="button">My Button 7</button>)).bind.innerHTML should be("<button type=\"button\">My Button 6</button><button type=\"button\">My Button 7</button>")
+    *            mySection(Array(<button type="button">My Button 8</button>, <button type="button">My Button 9</button>)).bind.innerHTML should be("<button type=\"button\">My Button 8</button><button type=\"button\">My Button 9</button>")
+    *            mySection(js.Array(<button type="button">My Button 10</button>, <button type="button">My Button 11</button>)).bind.innerHTML should be("<button type=\"button\">My Button 10</button><button type=\"button\">My Button 11</button>")
+    *            mySection(Constant(js.Array(<button type="button">My Button 12</button>, <button type="button">My Button 13</button>))).bind.innerHTML should be("<button type=\"button\">My Button 12</button><button type=\"button\">My Button 13</button>")
     *          }
     *
     *          test.watch()
     *          }}}
     *
-    *          `mySection` should not accept irrelevant parameter types like [[scala.List]] or [[scala.Int]].
+    *          `mySection` should not accept irrelevant parameter types like [[scala.collection.immutable.Set]] or [[scala.Int]].
     *
     *          {{{
-    *          "mySection(List.empty)" shouldNot compile
+    *          "mySection(Set.empty)" shouldNot compile
     *          "mySection(42)" shouldNot compile
     *          }}}
     */
